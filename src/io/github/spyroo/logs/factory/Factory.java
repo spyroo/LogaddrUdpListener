f354589f75e0c8d7df049fa3cf478e81bc6f5ad9
Spyroo <declan.l.johnson@gmail.com>
2015-06-24 19:31:52 -0500

    Maven Support

--- src/io/github/spyroo/logs/factory/Factory.java
+++
@@ -1,5 +0,0 @@
-package io.github.spyroo.logs.factory;
-
-public interface Factory<T, A> {
-    T create(A arg);
-}
--- src/io/github/spyroo/logs/structure/LogLineType.java
+++
@@ -1,7 +0,0 @@
-package io.github.spyroo.logs.structure;
-
-public enum LogLineType {
-    WORLD,
-    PLAYER_ACTION,
-    OTHER;
-}
